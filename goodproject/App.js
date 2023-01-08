b51b9de3f4a4fc2fd044cebd825c6393a904237e
Peter Cristian <100343267+Peter00125@users.noreply.github.com>
2023-01-08 16:56:14 +0200

    Add files via upload

--- goodproject/App.js
+++ goodproject/App.js
@@ -1,51 +1,116 @@
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal } from 'react-native';
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  ActivityIndicator
+} from 'react-native';
+import React from 'react';
 import colors from './Colors';
-import {AntDesign} from '@expo/vector-icons';
-import tempData from './tempData';
-import TodoList from './components/TodoList';
-import AddList from './components/AddList';
+import { AntDesign } from '@expo/vector-icons';
+import tempData from './tempData.js';
+import TodoList from './components/TodoList.js';
+import AddList from './components/AddList.js';
+import Fire from './Fire.js'
 
 export default class App extends React.Component {
-
   state = {
-    addTodoVisible: false
-  }
-  toggleAddtodoModal(){
-    this.setState({addTodoVisible: !this.state.addTodoVisible});
+    addTodoVisible: false,
+    lists: [],
+    user: {},
+    loading: true
+
+  };
+
+  componentDidMount(){
+    firebase = new Fire((error,user)=>{
+      if (error){
+        return alert("Error");
+      }
+        firebase.getLists(lists =>{
+          this.setState({lists,user},()=>{
+            this.setState({loading: false});
+          });
+        });
+        this.setState({user})
+    });
   }
 
-  render(){
-  return (
-    <View style={styles.container}>
-    <Modal animationType="slide" visible={this.state.addTodoVisible} onRequestClose={()=> this.toggleAddtodoModal}>
-      <AddList closeModal={()=>this.toggleAddtodoModal()}/>
-    </Modal>
-     <View style={{flexDirection: "row"}}>
-       <View style={styles.divider} />
-        <Text style={styles.title}>
-        Todo <Text style={{fontWeight:"300", color:colors.blue}}>List</Text>
-        </Text>
-         <View style={styles.divider} />
-    </View>
-    <View style={{marginVertical:48}}>
-      <TouchableOpacity style={styles.addList} onPress={()=> this.toggleAddtodoModal()}>
-      <AntDesign name="plus" size={16} color = {colors.blue} />
-      </TouchableOpacity>
-      <Text style={styles.add}>Add List</Text>
-    </View>
-    <View style={{height:275, paddingLeft:32}}>
-      <FlatList 
-      data={tempData}
-      keyExtractor={item => item.name}
-      horizontal={true}
-      showsHorizontalScrollIndicator={false}
-      renderItem={({item}) =><TodoList list = { item} />}
-       />
-    </View>
-    </View>
-  );
-}
+  componentWillUnmount(){
+    firebase.detach();
+
+  }
+
+  toggleAddtodoModal() {
+    this.setState({ addTodoVisible: !this.state.addTodoVisible });
+  }
+
+  renderList = list => {
+    return <TodoList list={list} updateList={this.updateList} />;
+  };
+
+  addList = list =>{
+    this.setState({lists: [...this.state.lists, {...list, id: this.state.lists.lenght +1, todos:[]}]});
+  };  
+
+  updateList = list =>{
+    this.setState({
+      lists: this.state.lists.map(item =>{
+        return item.id === list.id ? list :item;
+      })
+    })
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.container}>
+            <ActivityIndicator size = "large" color={colors.blue}/>
+        </View>
+      )
+    }
+    return (
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          visible={this.state.addTodoVisible}
+          onRequestClose={() => this.toggleAddtodoModal}>
+          <AddList closeModal={() => this.toggleAddtodoModal()} addList={this.addList} />
+        </Modal>
+        <View>
+          <Text>User:{this.state.user.uid}</Text>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo{' '}
+            <Text style={{ fontWeight: '300', color: colors.blue }}>List</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity
+            style={styles.addList}
+            onPress={() => this.toggleAddtodoModal()}>
+            <AntDesign name="plus" size={16} color={colors.blue} />
+          </TouchableOpacity>
+          <Text style={styles.add}>Add List</Text>
+        </View>
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={this.state.lists}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => this.renderList(item)}
+            keyboardShouldPersistTaps="always"
+          />
+        </View>
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
   container: {
@@ -54,30 +119,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  divider:{
+  divider: {
     backgroundColor: colors.lightBlue,
-    height:1,
-    flex:1,
-    alignSelf:"center"
+    height: 1,
+    flex: 1,
+    alignSelf: 'center',
   },
-  title:{
-    fontSize:38,
-    fontWeight:"800",
-    color:colors.black,
-    paddingHorizontal:64
+  title: {
+    fontSize: 38,
+    fontWeight: '800',
+    color: colors.black,
+    paddingHorizontal: 64,
   },
-  addList:{
-    borderWidth:2,
-    borderColor:colors.lightBlue,
-    borderRadius:4,
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
     padding: 16,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  add:{
-    color:colors.blue,
-    fontWeight:"600",
-    fontSize:14,
-    marginTop:8
-  }
+  add: {
+    color: colors.blue,
+    fontWeight: '600',
+    fontSize: 14,
+    marginTop: 8,
+  },
 });
